471c6194d8ba71171551a37bcce08addab959bf4
Alex <udinkik@mail.ru>
2023-02-21 20:14:09 +0300

    2

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 var redSquare = document.getElementById("red-square");
 var blueSquare = document.getElementById("blue-square");
 const pageHeight = document.body.clientHeight;
+
+let scrollHeight = Math.max(
+    document.body.scrollHeight, document.documentElement.scrollHeight,
+    document.body.offsetHeight, document.documentElement.offsetHeight,
+    document.body.clientHeight, document.documentElement.clientHeight
+  );
+let scrollWidth = Math.max(
+    document.body.scrollWidth, document.documentElement.scrollWidth,
+    document.body.offsetWidth, document.documentElement.offsetWidth,
+    document.body.clientWidth, document.documentElement.clientWidth
+);
 const scrollPosition = pageHeight / 2 - window.innerHeight / 2;
+
 window.scrollTo(0, scrollPosition);
 
 window.addEventListener("scroll", function() {
@@ -14,7 +26,7 @@ window.addEventListener("scroll", function() {
   var squareY = centerY + scrollY;
   redSquare.style.left = squareX + "px";
   redSquare.style.top = squareY + "px";
-  if (squareX <= 0 || squareX >= window.innerWidth || squareY <= 0 || squareY >= window.innerHeight) {
+  if (squareX <= 0 || squareX >= scrollWidth || squareY <= 0 || squareY >= scrollHeight) {
     redSquare.style.display = "none";
     blueSquare.style.left = squareX + "px";
     blueSquare.style.top = squareY + "px";
