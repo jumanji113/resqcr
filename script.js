0cdc0ee6c469389d5d0d2f6fb234824e8392cd5e
Alex <udinkik@mail.ru>
2023-02-22 10:21:54 +0300

    refresh css

--- script.js
+++ script.js
@@ -1,39 +1,22 @@
-var redSquare = document.getElementById("red-square");
-var blueSquare = document.getElementById("blue-square");
-const pageHeight = document.wrapper.clientHeight;
+const wrapper = document.querySelector('.wrapper');
+wrapper.scrollTop = wrapper.scrollHeight / 2 - wrapper.clientHeight / 2;
+wrapper.scrollLeft = wrapper.scrollWidth / 2 - wrapper.clientWidth / 2;
+const redSquare = document.querySelector('.red-square');
 
-let scrollHeight = Math.max(
-    document.wrapper.scrollHeight, document.documentElement.scrollHeight,
-    document.wrapper.offsetHeight, document.documentElement.offsetHeight,
-    document.wrapper.clientHeight, document.documentElement.clientHeight
-  );
-let scrollWidth = Math.max(
-    document.wrapper.scrollWidth, document.documentElement.scrollWidth,
-    document.wrapper.offsetWidth, document.documentElement.offsetWidth,
-    document.wrapper.clientWidth, document.documentElement.clientWidth
-);
-const scrollPosition = pageHeight / 2 - window.innerHeight / 2;
+function changeColor() {
+  redSquare.style.backgroundColor = 'blue';
+}
 
-window.scrollTo(0, scrollPosition);
+function resetColor() {
+  redSquare.style.backgroundColor = 'red';
+}
 
-window.addEventListener("scroll", function() {
-  var scrollX = window.scrollX;
-  var scrollY = window.scrollY;
-
-  var centerX = window.innerWidth / 2;
-  var centerY = window.innerHeight / 2;
-  var squareX = centerX + scrollX;
-  var squareY = centerY + scrollY;
-  redSquare.style.left = squareX + "px";
-  redSquare.style.top = squareY + "px";
-  if (squareX <= 0 || squareX >= scrollWidth || squareY <= 0 || squareY >= scrollHeight) {
-    redSquare.style.display = "none";
-    blueSquare.style.left = squareX + "px";
-    blueSquare.style.top = squareY + "px";
-    blueSquare.style.display = "block";
+wrapper.addEventListener('scroll', function() {
+  if (wrapper.scrollTop <= 0 || wrapper.scrollTop >= (wrapper.scrollHeight - wrapper.clientHeight)) {
+    changeColor();
+  } else if (wrapper.scrollLeft <= 0 || wrapper.scrollLeft >= (wrapper.scrollWidth - wrapper.clientWidth)) {
+    changeColor();
   } else {
-    redSquare.style.display = "block";
-    blueSquare.style.display = "none";
+    resetColor();
   }
-  console.log('Работа идет!');
 });
